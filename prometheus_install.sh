c8b06882a832e53154311f9789be27923a57a76f
gurge3 <gurge3@163.com>
2018-11-03 12:52:36 +0000

    update redis exporter and fix bug in bash file

--- prometheus_install.sh
+++ prometheus_install.sh
@@ -5,7 +5,8 @@ kubectl apply -f namespace.yaml
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 helm init
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-helm install coreos/prometheus-operator --name prometheus-operator --namespace monitoring
-helm install coreos/kube-prometheus --name kube-prometheus --set global.rbacEnable=true --namespace monitoring
+helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
+helm install coreos/prometheus-operator --name prometheus-koperator --namespace monitoring
+helm install coreos/kube-prometheus --name kube-prometheus --set global.rbakcEnable=true --namespace monitoring
 kubectl port-forward -n monitoring prometheus-kube-prometheus-0 9090
-kubectl port-forward $(kubectl get  pods --selector=app=kube-prometheus-grafana -n  monitoring --output=jsonpath="{.items..metadata.name}") -n monitoring  3000
+kubectl port-forward (kubectl get  pods --selector=app=kube-prometheus-grafana -n  monitoring --output=jsonpath="{.items..metadata.name}") -n monitoring  3000
